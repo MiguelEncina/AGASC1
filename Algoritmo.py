9d7fae1a6c9b38d48778a98cb532bff2483133c8
Miguel Encina <migencmar@alum.us.es>
2024-10-03 17:55:22 +0200

    feat: initialization completed

--- Algoritmo.py
+++ Algoritmo.py
@@ -1,9 +1,10 @@
 import numpy as np
 import random
+import math
 
 
-class AG_MOM():
-    def __init__(self, N, G, mut_op, cross_op, T, xl, xu):
+class AG_MOBJ():
+    def __init__(self, N, G, mut_op, cross_op, T, xl=0, xu=1):
         self.N = N
         self.G = G
         self.mut_op = mut_op
@@ -11,6 +12,13 @@ class AG_MOM():
         self.T = T
         self.xl = xl
         self.xu = xu
+        self.p = 30
+        self.m = 2
+        self.vectors = []
+        self.neightbors = []
+        self.pop = []
+        self.fobj = []
+        self.z = []
 
     def create_vectors(self):
         vectors = []
@@ -19,9 +27,9 @@ class AG_MOM():
             vector = [0+dst, 1-dst]
             vectors.append(vector)
             dst += dst
-        return vectors
+        self.vectors = vectors
 
-    def neighbors(self):
+    def find_neightbors(self):
         N = self.N
         vectors = self.create_vectors()
         dsts = np.zeros((N, N))
@@ -33,27 +41,51 @@ class AG_MOM():
         neighbors = []
 
         for i in range(N):
-            nearest_indexes = np.argsort(neighbors[i])[:self.T]
+            nearest_indexes = np.argsort(dsts[i])[:self.T]
             neighbors.append(nearest_indexes)
 
-        return neighbors
+        self.neightbors = neighbors
 
     def initialize_pop(self):
         pop = []
         for i in range(self.N):
             ind = []
-            for p in range(10):
-                ind.append(random.uniform(self.xl[p], self.xu[p]))
+            for p in range(self.p):
+                ind.append(random.uniform(self.xl, self.xu))
             pop.append(ind)
 
-        return pop
-    
+        self.pop = pop
+
+    def evaluate_pop(self):
+        fobj = []
+        for indv in self.pop:
+            tmp = 0
+            obj = [0, 0]
+            obj[0] = indv[0]
+            for i in range(1, self.N):
+                tmp += self.pop[i]
+
+            g = 1 + ((9 * tmp) / (self.N - 1))
+            h = 1 - math.sqrt(indv[0] / g) - (indv[0] / g) * math.sin(10 * math.pi * indv[0])
+            obj[1] = g * h
+            fobj.append(obj)
+
+        self.fobj = fobj
+
     def reference_point(self):
-        return
+        z = []
+        for i in range(self.m):
+            zi = min(self.fobj, key=lambda x:x[i])[i]
+            z.append(zi)
+        self.z = z
 
     def initialization(self):
-        return
-                
+        self.create_vectors()
+        self.find_neightbors()
+        self.initialize_pop()
+        self.evaluate_pop()
+        self.reference_point()
 
-ag = AG_MOM(1, 1, 1, 1, 1, 1, 1)
+
+ag = AG_MOBJ(1, 1, 1, 1, 1, 1, 1)
 ag.initialization()
